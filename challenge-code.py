7e5cc93abf65dffd9545249b11cc952a43767b30
mike15 <michail15@yahoo.com>
2023-03-25 15:45:51 -0400

    models testing

--- challenge-code.py
+++ challenge-code.py
@@ -3,8 +3,11 @@
 
 import pandas as pd
 
+from sklearn.feature_selection import RFE
+from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.linear_model import LinearRegression, Lasso, Ridge, LogisticRegression
 from sklearn.metrics import mean_squared_error
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split 
@@ -28,18 +31,6 @@ def dropColumns(df):
     'dd5-dd7 Aggregate Size Gradient',
     'Average DO concentration gradient d0',
     'Average DO concentration gradient d1',
-    'Average DO concentration gradient dd0',
-    'Average DO concentration gradient dd1',
-    'Average DO concentration gradient dd2',
-    'Average DO concentration gradient dd3',
-    'Average DO concentration gradient dd4',
-    'Average DO concentration gradient dd5',
-    'Average DO concentration gradient dd6',
-    'Average DO concentration gradient dd7',
-    'DO concentration/cell count dd0',
-    'DO concentration/cell count dd1',
-    'DO concentration/cell count dd2' ,
-    'DO concentration/cell count dd3',
     'DO gradient/cell count dd0',
     'DO gradient/cell count dd1',
     'DO gradient/cell count dd2',
@@ -58,7 +49,6 @@ def dropColumns(df):
     'dd3 DO 2nd derivative/cell count',
     'dd5 DO 2nd derivative/cell count',
     'dd7 DO 2nd derivative/cell count',
-    'Overall Average pH',
     'Overall density gradient',
     'Overall aggregate size gradient',
     'd0 Average pH Gradient',
@@ -78,41 +68,80 @@ def dropColumns(df):
     'dd7 Lactate Concentration',
     'Preculture Time [h]',
     'Start Preculture Perfusion [h after inoc] d1-d2',
-    'Presence of  IWP2 [h]'
+    'Presence of  IWP2 [h]',
+    'Average DO concentration d0'
     ])
 
 
 dropColumns(df_train)
 dropColumns(df_test)
 
-#scaler = StandardScaler()
-#X_std = scaler.fit_transform(df_train['x'])
 
+# Standardize the data
+scaler = StandardScaler()
+X_train_std = scaler.fit_transform(df_train['x'])
+X_test_std = scaler.fit_transform(df_test['x'])
+
+
+# Simple Linear Regression
 lm = LinearRegression()
-model1 = lm.fit(df_train['x'], df_train['y'])
-
-#X_train, X_test, y_train, y_test = train_test_split(df_train['x'], df_train['y'], test_size=0.2, random_state=0) 
-
-#y_test_pred = model1.predict(X_test)
-y_pred = model1.predict(df_test['x'])
-
-#mse1 = mean_squared_error(y_test, y_pred)
+model1 = lm.fit(X_train_std, df_train['y'])
+y_pred = model1.predict(X_test_std)
 mse1 = mean_squared_error(df_test['y'], y_pred)
-print(mse1)
+print("Linear Regression: ", mse1)
 x = df_test['x']
 y = df_test['y']
 
-print(metrics.accuracy_score(df_test['y'], y_pred))
-print(metrics.confusion_matrix(df_test['y'], y_pred))
 
-'''
+# Lasso Regression
+lasso = Lasso(alpha=1)
+model2 = lasso.fit(X_train_std, df_train['y'])
+y_test_pred = model2.predict(X_test_std)
+mse2 = mean_squared_error(df_test['y'], y_test_pred)
+print("Lasso: ", mse2)
+
+# Random Forest Regressor
+rfr = RandomForestRegressor(max_depth=(10), random_state=1, n_estimators=250)
+model3 = rfr.fit(X_train_std, df_train['y'])
+y_test_pred = model3.predict(X_test_std)
+mse3 = mean_squared_error(df_test['y'], y_test_pred)
+print("Random Forest Regressor: ", mse3)
+
+
+# Gradient Boosting Regressor
+gbt = GradientBoostingRegressor(random_state=0, n_estimators=200)
+model4 = gbt.fit(X_train_std, df_train['y'])
+y_test_pred = model4.predict(X_test_std)
+mse4 = mean_squared_error(df_test['y'], y_test_pred)
+print("Gradient Boosting Regressor: ", mse4)
+
+
+# Artificial Neural Network
+ann = MLPRegressor(hidden_layer_sizes=(15),max_iter=100000,random_state=0)
+model5 = ann.fit(X_train_std, df_train['y'])
+y_test_pred = model5.predict(X_test_std)
+mse5 = mean_squared_error(df_test['y'], y_test_pred)
+print("ANN: ", mse5)
+
+
+model = Lasso(alpha=0.01)
+model.fit(X_train_std,df_train['y'])
+model.coef_
+#pd.DataFrame(list(zip(X_train_std.columns,model.coef_)), columns = ['predictor','coefficient'])
+
+
+
+
 plt.figure(figsize=(10,6))
 
 
 print(x.shape[0])
 print(y.shape[0])
 
-plt.scatter(x=x['Presence of  IWP2 [h]'], y=y, color='g')
-plt.plot(df_test['x']['Presence of  IWP2 [h]'], y_pred,color='k') 
+#plt.scatter(x=y_test_pred, y=df_test['y'], color='g')
+#plt.plot(x['Overall Average pH'], y_pred,color='k') 
+plt.scatter(df_test['y'].index, df_test['y'].values, color='k')
+plt.scatter(y_test_pred.index, y_test_pred.values, color='h')
 plt.show()
-'''
+
+
